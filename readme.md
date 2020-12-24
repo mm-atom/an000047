c6b0efffd4024a7a8c5e80229e43f2393a2cee1a
taoqf <tao_qiufeng@126.com>
2020-12-24 14:54:38 +0800

    init

--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
-# 写cookie
+# 设置cookie
+
+在服务中写回cookie
+
+如果是https域名之间跨域请求时，必须设置选项`secure: true`
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,36 @@
-export default function fun() {
+import { ServerResponse } from 'http';
+import { serialize } from 'cookie';
+
+
+export interface CookieOptions {
+	maxAge?: number;
+	signed?: boolean;
+	expires?: Date;
+	httpOnly?: boolean;
+	path?: string;
+	domain?: string;
+	secure?: boolean;
+	encode?: (val: string) => string;
+	sameSite?: boolean | 'lax' | 'strict' | 'none';
+}
+
+export default function cookie(res: ServerResponse, name: string, value: string, options?: CookieOptions) {
+	const opts = {
+		...options
+	};
+
+	const val = typeof value === 'object'
+		? `j:${JSON.stringify(value)}`
+		: String(value);
+
+	if ('maxAge' in opts) {
+		opts.expires = new Date(Date.now() + opts.maxAge!);
+		opts.maxAge! /= 1000;
+	}
+
+	if (opts.path == null) {
+		opts.path = '/';
+	}
+
+	res.setHeader('Set-Cookie', serialize(name, String(val), opts));
 }
